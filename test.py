5b855ff3d92e2efb286ad5176c3901497b13c4b3
gemilang <git.gemilang.santiyuda@gmail.com>
2025-04-19 14:28:07 +0800

    tested

---
+++ test.py
@@ -0,0 +1,45 @@
+import json
+import pathlib
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from setup import prepare_args
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+    
+    
+
+def test(model):
+    X_test, y_test = pd.read_csv("X_test.csv").drop(columns=['Unnamed: 0']), pd.read_csv("y_test.csv").drop(columns=['Unnamed: 0'])
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    scores = {'MSE': mse,'RMSE': rmse,'MAE': mae}
+    return scores
+    
+def combine_with_tuned_params(model_name, default_params):
+    if model_name == "linear":
+        return {}
+    tuned_params_filepath = pathlib.Path()/"models"/f"{model_name}_params.json"
+    tuned_params = {}
+    with open(tuned_params_filepath.absolute(), "r") as json_file:
+        tuned_params = json.load(json_file)
+    for k,v in tuned_params.items():
+        default_params[k] = v
+    return default_params
+
+
+if __name__ == "__main__":
+    args = prepare_args()
+    trained_model = None
+    save_path = pathlib.Path()/"models"/f"{args.model}_model.pkl"
+    with open(save_path.absolute(), "rb") as f:
+        trained_model = pickle.load(f)
+    score_dict = test(trained_model)
+    test_result_filepath = pathlib.Path()/"test_results.csv"
+    
+    with open(test_result_filepath.absolute(), "a+") as result_file:
+        result_file.write(f"{args.model},{score_dict['RMSE']},{score_dict['MSE']},{score_dict['MAE']}\n")
+    
---
+++ test_caller.py
@@ -0,0 +1,52 @@
+from subprocess import run
+
+PREPROCESS_MODES = [
+                    "p1", 
+                    "p2", 
+                    "p3", 
+                    "p4", 
+                    "p5", 
+                    "p6", 
+                    "p7", 
+                    "p8", 
+                    "p9"
+                    ]
+MODEL_CHOICES = [
+            "catboost",
+            "xgboost",
+            "lightgbm",
+            "linear_regression",
+            "lasso",
+            "ridge",
+            "elasticnet",
+            "svm",
+            "random_forest",
+            "mlp"
+        ]
+
+def call_test(model, preprocess_mode, use_rfe):
+    cmd_args = ["python",
+         "test.py",
+         "--preprocess-mode",
+         preprocess_mode,
+         "--model",
+         model]
+    if use_rfe:
+        cmd_args.append("--use-rfe")
+    run(cmd_args)
+
+if __name__ == "__main__":
+    model_ppmode_list = [
+    ("xgboost", "p1", False),
+    ("catboost", "p1", False),
+    ("elasticnet", "p7", False),
+    ("lasso", "p1", True ),
+    ("lightgbm", "p9", False),
+    ("random_forest", "p5", False),
+    ("ridge", "p4", True ),
+    ("svm", "p9", False),
+    ("mlp", "p9", False )]
+    
+    for args in model_ppmode_list:
+        print(args)
+        call_test(*args)
